29470d9c6230043496db4bf237bbb4dede6eede3
DiogenSin <jakasdovydas@gmail.com>
2023-01-02 12:58:53 +0200

    Atlikta septinta užduotis

--- Task 7/script.js
+++ Task 7/script.js
@@ -10,3 +10,9 @@ const audi = {
   year: 2005,
   color: 'white'
 };
+
+function showObjectKeys(array){
+  let objectKeys = Object.keys(array)
+  return objectKeys
+}
+console.log(showObjectKeys(audi))
