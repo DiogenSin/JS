7d5adc3a562b774792cbec15f450e9c6885f1f2c
DiogenSin <jakasdovydas@gmail.com>
2023-01-02 13:17:06 +0200

    Atlikta aštunta užduotis

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,29 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator{
+    constructor(skaicius1, skaicius2){
+    this.skaicius1 = skaicius1,
+    this.skaicius2 = skaicius2
+    }
+    sum(){
+        return this.skaicius1+this.skaicius2
+    }
+    substract(){
+        return this.skaicius1 - this.skaicius2
+    }
+    multiplication(){
+        return this.skaicius1*this.skaicius2
+    }
+    division(){
+        return this.skaicius1/this.skaicius2
+    }
+}
+
+let skaiciai = new Calculator(3, 9)
+
+console.log(skaiciai.sum())
+console.log(skaiciai.substract())
+console.log(skaiciai.multiplication())
+console.log(skaiciai.division())
